7d95bcb1587b225268d9eea6f5e42795ef0da756
yamachoo <51016095+yamachoo@users.noreply.github.com>
2022-01-15 16:42:56 +0900

    Add StringSpecTests

---
+++ src/test/kotlin/StringSpecTests.kt
@@ -0,0 +1,12 @@
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class StringSpecTests : StringSpec({
+    "strings.length should return size of string" {
+        "hello".length shouldBe 5
+    }
+
+    "this test is disabled".config(enabled = false) {
+        // test here
+    }
+})
