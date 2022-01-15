a42e5bddbc2a2c5456441e011fc5cf46a29acd46
yamachoo <51016095+yamachoo@users.noreply.github.com>
2022-01-15 16:12:02 +0900

    Add FunSpecTests

---
+++ src/test/kotlin/FunSpecTests.kt
@@ -0,0 +1,27 @@
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class FunSpecTests : FunSpec({
+    context("Test 1") {
+        context("Test 1-1") {
+            test("String length should return the length of the string") {
+                "sammy".length shouldBe 5
+            }
+        }
+
+        test("String length should return the length of the string") {
+            "".length shouldBe 0
+        }
+
+        xtest("this test is disabled") {
+            // test here
+        }
+    }
+
+    xcontext("Test 2") {
+        test("String length should return the length of the string") {
+            "sammy".length shouldBe 5
+            "".length shouldBe 0
+        }
+    }
+})
