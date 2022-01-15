4e88fb910c3ae4a01c5d7738bfe84a71795d3830
yamachoo <51016095+yamachoo@users.noreply.github.com>
2022-01-15 19:13:21 +0900

    Add AnnotationSpecTests

---
+++ src/test/kotlin/AnnotationSpecTests.kt
@@ -0,0 +1,19 @@
+import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.matchers.shouldBe
+
+class AnnotationSpecTests : AnnotationSpec() {
+    @BeforeEach
+    fun beforeTest() {
+        println("Before each test")
+    }
+
+    @Test
+    fun test1() {
+        1 shouldBe 1
+    }
+
+    @Test
+    fun test2() {
+        3 shouldBe 3
+    }
+}
